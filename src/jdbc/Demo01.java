2521f1fa5fb3bdc3f511853f9e63cafd6912ff6c
TWM <480086311@qq.com>
2018-03-13 14:47:11 +0800

    Hello JDBC

--- src/jdbc/Demo01.java
+++ src/jdbc/Demo01.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class TestJDBC {
+public class Demo01 {
 	public static void main(String[] args) {
 		Connection c = null;
 		Statement s = null;
